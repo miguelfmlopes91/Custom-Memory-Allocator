d8b167b1d1e2ad743d44f9a8deb8b14c4c66e677
Miguel Lopes <miguel.lopes@miniclip.com>
2019-11-19 23:49:14 +0000

    add classes

--- Accelerated cpp/main.cpp
+++ Accelerated cpp/main.cpp
@@ -2,14 +2,21 @@
 
 #include "str.h"
 
-int main(){
-    
-    Str str("lol");
-//    Vec<int> vec(10);
-//    for (int i = 0; i< vec.size(); ++i) {
-//        std::cout << vec[i] << std::endl;
-//    }
-    Str temp = str + "ajaa";
-    std::cout<<str << std::endl;
+#include "memmgr.h"
+#include "classes.h"
+
+int main(int argc, char* argv[])
+{
+    for(long i = 0; i < 500000; ++i)
+    {
+        JobScheduler* scheduler = new JobScheduler;
+        Coordinate*   point  = new Coordinate;
+        Complex*      number = new Complex;
+        
+        delete scheduler;
+        delete point;
+        delete number;
+    }
     return 0;
 }
+
